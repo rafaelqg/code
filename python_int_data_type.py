21be31776d20d910b54be69857f65fb946806c2e
Rafael Queiroz Gonçalves <rafael.q.g@hotmail.com>
2021-05-05 08:35:34 -0300

    Create python_int_data_type.py

---
+++ python_int_data_type.py
@@ -0,0 +1,15 @@
+intMaxJava= 2147483647 # maximum positive int (4 bytes varible)
+print (intMaxJava)
+print (type(intMaxJava))
+
+longMaxJava= 9223372036854775807 #maximum positive long (8 bytes variable)
+print (longMaxJava)
+print (type(longMaxJava))
+
+pythonIntUnboundery=intMaxJava+longMaxJava
+print (pythonIntUnboundery)
+print (type(pythonIntUnboundery))
+
+pythonIntUnboundery=pythonIntUnboundery*pythonIntUnboundery
+print (pythonIntUnboundery)
+print (type(pythonIntUnboundery))
