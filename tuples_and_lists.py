85495a104d6ec56bf8fce281ca3f23e9e84e5d6b
Rafael Queiroz Gonçalves <rafael.q.g@hotmail.com>
2021-06-21 21:31:56 -0300

    Create tuples_and_lists.py

---
+++ tuples_and_lists.py
@@ -0,0 +1,32 @@
+list=[]
+tuple=()
+
+print(type(list))
+print(type(tuple))
+
+
+tuple=("Legolas","Frodo", "Aragorn")
+print(tuple[0])
+print(tuple[1])
+print(tuple[2])
+#tuple[0]="Gandolf"
+tuple.append("Gandolf")
+for x in tuple:
+    print("tuple element:",x)
+
+
+list=["Legolas","Frodo", "Aragorn"]
+print(list[0])
+print(list[1])
+print(list[2])
+list.append("Gandolf")
+for x in list:
+    print("list element:", x)
+
+
+
+
+#list.append("Gandolf")
+#tuple.append("Gandolf")
+
+
