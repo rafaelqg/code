0fe3d022238da279f1ec25047bd60e21ff0b864a
Rafael Queiroz Gonçalves <rafael.q.g@hotmail.com>
2022-07-18 06:40:55 -0300

    Update nodejs_mongodb_heteregeneous_schemas.js

--- nodejs_mongodb_heteregeneous_schemas.js
+++ nodejs_mongodb_heteregeneous_schemas.js
@@ -1,17 +1,43 @@
 const {MongoClient} = require('mongodb');
-const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri);
+const uri = "mongodb://localhost:27017";//$ mongo mongodb://<host>:<port>
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
 client.connect().then(function(){
-    let db = client.db("company");
-    let collection = db.collection('objects');
-    let listOfObjects = [
-        {description:"ObjectA"},
-        {descriptionB:"ObjectB"},
-        {descriptionC:"ObjectC"}
-    ];
-    const options = {ordered:true};//preventsinsertionifonefails
-    collection.insertMany(listOfObjects, options).then(function(result){
-        console.log("Result",result);
+        let db=client.db("company");//access or create a new database
+        let collection = db.collection('employees');// access or create a new collection
+        //insert objects
+        let employee = {
+            name: "Mickey",
+            lastName: "Mouse"
+        }    
+        
+        collection.insertOne(employee, function(err, res) { //add document to collection
+            if (err != undefined) throw err;
+            console.log("1 document inserted");
+        });
+
+        employee={
+            fullName: "Donald Duck",
+        }    
+        
+        collection.insertOne(employee, function(err, res) { //add document to collection
+            if (err != undefined) throw err;
+            console.log("1 document inserted");
+        });
+        let queryObject={};
+
+        let cursor=collection.find(queryObject);
+        cursor.toArray().then( 
+            function(objects){
+                console.log("find() output:", objects);
+                objects.forEach(element => {
+                    if(element.fullName !== undefined){
+                        console.log("I have the fullName attribute",element);
+                    }else{
+                        console.log("I do not have the fullName attribute",element);
+                    }
+                });
+            },function(){}
+        );
     });
-    }
-);
+    
